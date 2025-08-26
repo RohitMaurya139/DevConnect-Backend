9fbf02519f01131b791616d00b761db247d6ab6e
RohitMaurya139 <rohit139maurya@gmail.com>
2025-08-27 01:12:38 +0530

    update 27 Aug 2025

--- src/app.js
+++ src/app.js
@@ -1,24 +1,62 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
+app.get(/^\/ab?c$/, (req, res) => {
+  res.send({
+    firstName: "Rocky",
+    lastName: "Maurya",
+    email: "rocky194maurya@gmail.com",
+  });
+});
+app.get(/^\/ab+c$/, (req, res) => {
+  res.send({
+    firstName: "Prem",
+    lastName: "Maurya",
+    email: "prem098maurya@gmail.com",
+  });
+});
+app.get(/^\/ab.*cd$/, (req, res) => {
+  res.send({
+    firstName: "Akky",
+    lastName: "Maurya",
+    email: "akky456maurya@gmail.com",
+  });
+});
+
+app.get(/^\/a(bc)?d$/, (req, res) => {
+  res.send({
+    firstName: "gkky",
+    lastName: "Maurya",
+    email: "akky456maurya@gmail.com",
+  });
+});
+
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)
+    // console.log(req.query)
+  res.send({
+    firstName: "Rohit",
+    lastName: "Maurya",
+    email: "rohit139maurya@gmail.com", // fixed typo "emsil"
+  });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Saved data to the database");
+  res.send("Data successfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully from database");
+});
+
+// Use app.get here if you want to handle only GET requests on /test
+app.get("/test", (req, res) => {
   res.send("Hello From test");
 });
-app.use("/rohit", (req, res) => {
-  res.send("Hello From rohit");
-});
-app.use("/piki", (req, res) => {
-  res.send("Hello From piki");
-});
-app.use("/raju", (req, res) => {
-  res.send("Hello From raju");
-});
 
-
-app.use( (req, res) => {
-  res.send("Hello From dashBoard");
-});
+// Removed app.use("/user") middleware that conflicts with user routes
 
 app.listen(3000, () => {
-  console.log("server succesFully listening port 3000");
+  console.log("server successfully listening on port 3000");
 });
