6734f195ffd52f4a12de4cffcddc11293d61c019
RohitMaurya139 <rohit139maurya@gmail.com>
2025-08-27 13:39:23 +0530

    multiple-handler

---
+++ src/app1.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const app = express();
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    // res.send("route handler 1")
+    console.log("route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    //  res.send("route handler 2");
+    console.log("route handler 2");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("route handler 3");
+    console.log("route handler 3");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("route handler 4");
+    console.log("route handler 4");
+    next();
+  },
+  (req, res, next) => {
+    res.send("route handler 5");
+    console.log("route handler 5");
+    
+  });
+
+app.listen(2000, () => {
+  console.log("server successfully listening on port 2000");
+});
