a4a9683bb68db8834417e3c5623c1b57d878fe0d
RohitMaurya139 <rohit139maurya@gmail.com>
2025-09-15 14:28:24 +0530

    fix cors 2

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     origin:
-      "https://dev-connect-client-mu.vercel.app/",
+      "https://dev-connect-client-mu.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
